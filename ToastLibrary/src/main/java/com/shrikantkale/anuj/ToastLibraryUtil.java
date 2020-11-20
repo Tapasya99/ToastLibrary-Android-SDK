14dd6b26eceb0b74ce026e34422872e894919c63
Tapasya <tapasya@datability.co>
2020-11-20 10:15:24 +0530

    Code update

--- ToastLibrary/src/main/java/com/shrikantkale/anuj/ToastLibraryUtil.java
+++ ToastLibrary/src/main/java/com/shrikantkale/anuj/ToastLibraryUtil.java
@@ -1,10 +1,12 @@
 package com.shrikantkale.anuj;
 
 import android.content.Context;
+import android.util.Log;
 import android.widget.Toast;
 
 public class ToastLibraryUtil {
     public static void showToast(Context context, String message){
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+        Log.e("TAG", "VersionName: 1.0.2" );
     }
 }
